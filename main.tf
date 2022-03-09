7212db46849e1968d4d483c6a555d7eb67c4e5a9
Matt Kazmar <mkazmar@aviatrix.com>
2022-03-09 17:07:05 -0600

    update 3

--- main.tf
+++ main.tf
@@ -4,64 +4,64 @@
 data "google_client_config" "cc" {}
 
 locals {
-  project                                   = var.project == "" ? data.google_client_config.cc.project : var.project
-  region                                    = var.region == "" ? data.google_client_config.cc.region : var.region
-  zone                                      = var.zone == "" ? data.google_client_config.cc.zone : var.zone
-  public_ip_name                            = var.public_ip_name == "" ? "${var.vm_name}-public-ip" : var.public_ip_name
-  public_ip                                 = var.use_existing_public_ip ? data.google_compute_address.avx_copilot_publicip.address : google_compute_address.avx_copilot_publicip.address
-  data_disk_name                            = var.data_disk_name == "" ? "${var.vm_name}-data-disk-1" : var.data_disk_name
+  project        = var.project == "" ? data.google_client_config.cc.project : var.project
+  region         = var.region == "" ? data.google_client_config.cc.region : var.region
+  zone           = var.zone == "" ? data.google_client_config.cc.zone : var.zone
+  public_ip_name = var.public_ip_name == "" ? "${var.vm_name}-public-ip" : var.public_ip_name
+  public_ip      = var.use_existing_public_ip ? data.google_compute_address.avx_copilot_publicip.address : google_compute_address.avx_copilot_publicip.address
+  data_disk_name = var.data_disk_name == "" ? "${var.vm_name}-data-disk-1" : var.data_disk_name
 }
 
 resource "google_compute_address" "avx_copilot_publicip" {
-    count                                   = var.use_existing_public_ip ? 0 : 1
-    name                                    = var.public_ip_name
-    address_type                            = "EXTERNAL"
-    project                                 = local.project
-    region                                  = local.region
+  count        = var.use_existing_public_ip ? 0 : 1
+  name         = var.public_ip_name
+  address_type = "EXTERNAL"
+  project      = local.project
+  region       = local.region
 }
 
 data "google_compute_address" "avx_copilot_publicip" {
-  count                                   = var.use_existing_public_ip ? 1 : 0
-  name                                    = local.public_ip_name
-  project                                 = local.project
-  region                                  = local.region
+  count   = var.use_existing_public_ip ? 1 : 0
+  name    = local.public_ip_name
+  project = local.project
+  region  = local.region
 }
 
 data "google_compute_subnetwork" "avx_copilot_subnet" {
-  name                                    = var.subnet_name
-  project                                 = local.project
-  region                                  = local.region
+  name    = var.subnet_name
+  project = local.project
+  region  = local.region
 }
 
 resource "google_compute_disk" "data_disk" {
-  name                                      = local.data_disk_name
-  size                                      = var.data_disk_size
-  type                                      = "pd-ssd"
+  name = local.data_disk_name
+  size = var.data_disk_size
+  type = "pd-ssd"
 }
 
 
 resource "google_compute_instance" "avx_copilot_vm" {
-    name                                    = var.vm_name
-    machine_type                            = var.vm_size #"n2-standard-2" # https://docs.aviatrix.com/HowTos/copilot_getting_started.html#instance-system-requirements
-    zone                                    = local.zone
-    tags                                    = var.tags
-    allow_stopping_for_update               = true
+  name                      = var.vm_name
+  machine_type              = var.vm_size #"n2-standard-2" # https://docs.aviatrix.com/HowTos/copilot_getting_started.html#instance-system-requirements
+  zone                      = local.zone
+  tags                      = var.tags
+  allow_stopping_for_update = true
 
-    boot_disk {
-        initialize_params  {
-            image                           = "aviatrix-public/avx-copilot-gcp-1-6-1-2022-01-27"
-        }
+  boot_disk {
+    initialize_params {
+      image = "aviatrix-public/avx-copilot-gcp-1-6-1-2022-01-27"
     }
+  }
 
-    network_interface {
-        subnetwork                          = data.google_compute_subnetwork.extranet-avx-controller.self_link
-        access_config {
-            nat_ip                          = local.public_ip
-        }
+  network_interface {
+    subnetwork = data.google_compute_subnetwork.extranet-avx-controller.self_link
+    access_config {
+      nat_ip = local.public_ip
     }
+  }
 
-    attached_disk {
-      source = google_compute_disk.data_disk.self_link
-    }
+  attached_disk {
+    source = google_compute_disk.data_disk.self_link
+  }
 
 }
--- variables.tf
+++ variables.tf
@@ -1,65 +1,65 @@
-variable project {
+variable "project" {
   description = "Project to deploy into. Leave at the default to use the provider defaults."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
-variable region {
+variable "region" {
   description = "Region to deploy into. Leave at the default to use the provider defaults."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
-variable zone {
+variable "zone" {
   description = "Zone to deploy into. Leave at the default to use the provider defaults."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
-variable use_existing_public_ip {
+variable "use_existing_public_ip" {
   description = "Use an existing public IP. True/false."
-  type = string
-  default = false
+  type        = string
+  default     = false
 }
 
-variable public_ip_name {
+variable "public_ip_name" {
   description = "The name of the Public IP to use or deploy. Don't specify to use the default name."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
-variable subnet_name {
+variable "subnet_name" {
   description = "The name of the existing subnet to deploy into. This must be in the same region as the VM."
-  type = string
+  type        = string
 }
 
-variable data_disk_size {
+variable "data_disk_name" {
   description = "The name of the data disk. Leave as default to autogenerate a name."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
-variable data_disk_size {
+variable "data_disk_size" {
   description = "The size of the data disk in GB to deploy for storage."
-  type = number
-  default = 2000
+  type        = number
+  default     = 2000
 }
 
-variable tags {
+variable "tags" {
   description = "Any Network tags needed on the VM."
-  type = list(string)
-  default = null
+  type        = list(string)
+  default     = null
 }
 
-variable vm_name {
+variable "vm_name" {
   description = "The Copilot VM's name."
-  type = string
-  default = "aviatrix-copilot"
+  type        = string
+  default     = "aviatrix-copilot"
 }
 
-variable vm_size {
+variable "vm_size" {
   description = "The Copilot VM's size. Minimum is 2cpu/4gb. n2-standard-2 is 2cpu/8gb."
-  type = string
-  default = "n2-standard-2"
+  type        = string
+  default     = "n2-standard-2"
 }
 
